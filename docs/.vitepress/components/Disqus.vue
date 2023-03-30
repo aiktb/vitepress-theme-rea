cce8625b462c3e467e2577f6e338b7ef12f26d24
aiktb <ai.ourship@gmail.com>
2023-03-30 21:59:48 +0800

    ignore

---
+++ docs/.vitepress/components/Disqus.vue
@@ -0,0 +1,10 @@
+<script>
+import {Disqus} from 'vue-disqus'
+
+export default {
+  name: 'PostPage',
+  components: {
+    Disqus
+  }
+}
+</script>
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vitepress'
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
     lang: "zh-CN",
     title: "aiktb",
@@ -8,23 +7,13 @@ export default defineConfig({
     cleanUrls: true,
     themeConfig: {
         logo: "/logo.png",
-        // https://vitepress.dev/reference/default-theme-config
+        darkMode: false,
         nav: [
             {text: 'Home', link: '/'},
-            {text: 'About', link: '/about'},
+            {text: 'About', link: '/about.md'},
             {text: 'Archive', link: '/archive'},
             {text: 'Collection', link: '/collection'},
             {text: 'ACG', link: '/acg'},
-        ],
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/aiktb' },
-            { icon: 'twitter', link: 'https://twitter.com/aiktb39' },
-            {
-                icon: {
-                    svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>telegram</title><path d="/telegram_logo.svg"/></svg>'
-                },
-                link: 'https://t.me/aiktb'
-            }
         ],
 
         head: [
@@ -35,5 +24,6 @@ export default defineConfig({
             message: 'All articles on this website are licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target = "_blank" >CC BY 4.0</a>.',
             copyright: 'Copyright © 2023 aiktb. Powered by <a href="https://vitepress.dev/" target = "_blank" >VitePress</a>.',
         }
-    }
+    },
+
 })
