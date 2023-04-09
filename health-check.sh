fb5caf1a92d21cf1cc95a4712b2361973e4ddb03
aiktb <aiktb@outlook.com>
2023-04-09 18:26:31 +0800

    Fix health-check.sh bug: get pid

--- health-check.sh
+++ health-check.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 cd /app || exit
+git fetch
 local_commit=$(git rev-parse HEAD)
 remote_commit=$(git rev-parse origin/HEAD)
 if [ "$local_commit" != "$remote_commit" ]; then
-    kill 1
+    pid=$(pgrep -f "npm run docs:preview")
+    kill "$pid"
 fi
