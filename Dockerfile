cbc86efac85cb85a14b8036a4c4907da820c8c02
aiktb <aiktb@outlook.com>
2023-04-09 19:04:14 +0800

    Fix bug: ambiguous argument 'origin/HEAD'

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /app
 COPY . .
 RUN apk add git
 RUN git pull
+
 RUN npm install
 RUN npm run docs:build
 HEALTHCHECK --interval=30s --timeout=12s --start-period=30s CMD ["/bin/sh", "/app/health-check.sh"]
--- health-check.sh
+++ health-check.sh
@@ -2,8 +2,8 @@
 
 cd /app || exit
 git fetch
-local_commit=$(git rev-parse HEAD)
-remote_commit=$(git rev-parse origin/HEAD)
+local_commit=$(git rev-parse master)
+remote_commit=$(git rev-parse origin/master)
 if [ "$local_commit" != "$remote_commit" ]; then
     pid=$(pgrep -f "npm run docs:preview")
     kill "$pid"
