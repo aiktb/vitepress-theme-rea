15c0ce1567038632f9f9509711230cade2b44d23
aiktb <aiktb@outlook.com>
2023-04-24 22:52:24 +0800

    Update package-lock.json(npm i)

--- docs/.vitepress/theme/components/Comments.vue
+++ docs/.vitepress/theme/components/Comments.vue
@@ -1,25 +1,25 @@
 <script setup>
-import Giscus from '@giscus/vue'
-import { useData } from 'vitepress'
+import Giscus from '@giscus/vue';
+import { useData } from 'vitepress';
 
 const {title} = useData()
 </script>
 
 <template>
-    <div class="comments" :key="title">
-      <Giscus
-          repo="aiktb/rea"
-          repo-id="R_kgDOJQkw9Q"
-          category="Announcements"
-          category-id="DIC_kwDOJQkw9c4CVeXb"
-          mapping="pathname"
-          strict="0"
-          reactions-enabled="1"
-          emit-metadata="0"
-          input-position="bottom"
-          lang="en"
-          theme="transparent_dark"
-          loading="eager"
-      />
-    </div>
+  <div class="comments" :key="title">
+    <Giscus
+        repo="aiktb/rea"
+        repo-id="R_kgDOJQkw9Q"
+        category="Announcements"
+        category-id="DIC_kwDOJQkw9c4CVeXb"
+        mapping="pathname"
+        strict="0"
+        reactions-enabled="1"
+        emit-metadata="0"
+        input-position="bottom"
+        lang="en"
+        theme="transparent_dark"
+        loading="eager"
+    />
+  </div>
 </template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,8 @@
 // .vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
-import Layout from './components/Layout.vue'
+import Layout from './components/Layout.vue';
 
 import './custom.css'
+
 export default {
-    ...DefaultTheme,
     Layout: Layout,
 }
