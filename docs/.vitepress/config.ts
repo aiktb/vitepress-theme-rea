4ffe92b58f4cc4c811e9471d4bf3bb17aff2bfc7
aiktb <aiktb@outlook.com>
2023-05-23 19:00:27 +0800

    style: change chinese font

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
         [
             'style',
             {},
-            `@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&display=swap');`
+            `@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;700&display=swap');`
         ],
         [
             'style',
